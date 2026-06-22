35f04818c4846afd1ffd90d0fcf50881e596a37b
Tema82Rus <Fenix23245@yandex.ru>
2026-06-22 13:00:30 +0300

    Сверстана коллекция для отображения списка валют

---
+++ FakeNFT/Scenes /Cart/Cells/PaymentCell.swift
@@ -0,0 +1,12 @@
+//
+//  PaymentCell.swift
+//  FakeNFT
+//
+//  Created by Artem Yaroshenko on 21.06.2026.
+//
+
+import UIKit
+
+class PaymentCell: UICollectionViewCell, ReuseIdentifying {
+    
+}
---
+++ FakeNFT/Scenes /Cart/VC/PaymentViewController.swift
@@ -0,0 +1,81 @@
+//
+//  PaymentViewController.swift
+//  FakeNFT
+//
+//  Created by Artem Yaroshenko on 21.06.2026.
+//
+
+import UIKit
+
+class PaymentViewController: UIViewController {
+    // MARK: - Public Properties
+    
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumInteritemSpacing = 7
+        layout.minimumLineSpacing = 7
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(PaymentCell.self)
+        return collectionView
+    }()
+    
+    // MARK: - Private Properties
+    
+    
+    
+    // MARK: - View LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupCollectionView()
+    }
+    
+    // MARK: - Public Methods
+    
+    
+    
+    // MARK: - Private Methods
+    private func setupCollectionView() {
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        view.addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0),//TODO: изменить после добавления нижнего блока
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+        ])
+    }
+    
+}
+
+extension PaymentViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        8
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: PaymentCell = collectionView.dequeueReusableCell(indexPath: indexPath)
+        cell.backgroundColor = .systemRed
+        return cell
+    }
+    
+    
+}
+
+extension PaymentViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let spacing: CGFloat = 7
+        let cell: CGFloat = 2
+        let totalSpacing = spacing * (cell - 1)
+        let availableWidth = collectionView.frame.width - totalSpacing
+        let widthPerItem = availableWidth / cell
+        return CGSize(width: widthPerItem, height: 46)
+    }
+}
+
+#Preview {
+    PaymentViewController()
+}
